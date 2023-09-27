0d52029dc36555d852fe9ba5ce08123a5b7dee99
Miguel Vargas <mvarlic@gmail.com>
2023-09-27 14:24:29 -0500

    feat: add constants

---
+++ src/util/Constans.js
@@ -0,0 +1,7 @@
+export default class Constans {
+    static MONTHS_OF_YEAR = ['Enero', 'Febrero', 'Marzo', 
+        'Abril', 'Mayo', 'Junio', 
+        'Julio', 'Agosto', 'Setiempre', 
+        'Octubre', 'Noviembre', 'Diciembre'];   
+}
+
