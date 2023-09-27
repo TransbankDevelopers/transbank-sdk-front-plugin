057c1cce4226e4ba485db7066166f9eeb2488a1c
Miguel Vargas <mvarlic@gmail.com>
2023-09-27 14:25:34 -0500

    feat: add tab constants util

---
+++ src/util/TabConstans.js
@@ -0,0 +1,15 @@
+export default class TabConstans {
+    static DASHBOARD = 0;
+    static WEBPAYPLUS = 1;
+    static WEBPAYPLUS_MALL = 2;
+    static ONECLICK = 3;
+    static TRANSACTION = 4;
+    static INSCRIPTION = 5;
+    static API_SERVICE_LOG = 6;
+    static EXECUTION_ERROR_LOG = 7;
+    static DIAGNOSTICO = 8;
+    static LOGS = 9;
+    static PHPINFO = 10;
+    static CONTACT = 11;
+}
+
